a8afc5e1ac342a4bdb67e629c3ef25d4189d3e84
Flavio <flavio_nz@outlook.com>
2025-09-03 12:07:30 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-0d1fa7d8-2fca-40f0-b2ce-1d1c3e05109f.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
